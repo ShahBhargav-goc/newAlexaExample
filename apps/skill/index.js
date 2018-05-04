b8c50c82fffa6b6ee3538e25e9912caf7c872bbc
ShahBhargav-goc <bhargav.goc@gmail.com>
2018-05-04 16:05:16 +0530

    update

--- apps/skill/index.js
+++ apps/skill/index.js
@@ -23,24 +23,31 @@ app.error = function( exception, request, response ) {
 app.intent('sObject',
   {
 	"slots":{"date":"AMAZON.DATE",
-			 "toObject":"toObject"}
+			 "toObject":"toObject",
+			 "hotJob":"hotJob",
+			 "picklistValue":"picklistValue",
+		     "fieldName":"fieldName"}
 	,"utterances":[ 
 		"what are {date} open {toObject}"]
   },
   function(request,response) {
 	var date = request.slot('date');
 	var toObject = request.slot('toObject');
+	var hotJob  = request.slot('hotJob');
+	var picklistValue  = request.slot('picklistValue');
+	var fieldName  = request.slot('fieldName');
     //response.say("You asked for the number " + jobnumber);
 
-    if (_.isEmpty(date)) {
-      	var prompt = 'I didn\'t hear a Date. Tell me a Date.';
-      	response.say(prompt).reprompt(reprompt).shouldEndSession(false);
-      	return true;
-    } else if(_.isEmpty(toObject)) {
-		var prompt = 'I didn\'t hear a Object. Tell me a Object.';
-      	response.say(prompt).reprompt(reprompt).shouldEndSession(false);
-      	return true;
-	} else if(!_.isEmpty(date) && !_.isEmpty(toObject)) {
+    // if (_.isEmpty(date)) {
+    //   	var prompt = 'I didn\'t hear a Date. Tell me a Date.';
+    //   	response.say(prompt).reprompt(reprompt).shouldEndSession(false);
+    //   	return true;
+    // } else if(_.isEmpty(toObject)) {
+	// 	var prompt = 'I didn\'t hear a Object. Tell me a Object.';
+    //   	response.say(prompt).reprompt(reprompt).shouldEndSession(false);
+    //   	return true;
+	// } else 
+	if(!_.isEmpty(date) && !_.isEmpty(toObject) && _.isEmpty(hotJob) && _.isEmpty(picklistValue) && _.isEmpty(fieldName)) {
     	try{
 			var request = require('sync-request');
 			var res = request('GET', ENDPOINT +'?Date='+date+'&toObject='+toObject,{
@@ -56,6 +63,21 @@ app.intent('sObject',
 		}
       	return true;
 
+	}else if(!_.isEmpty(hotJob) && !_.isEmpty(toObject) && _.isEmpty(date) && _.isEmpty(picklistValue) && _.isEmpty(fieldName)) {
+		try{
+			var request = require('sync-request');
+			var res = request('GET', ENDPOINT +'?hotJob='+hotJob+'&toObject='+toObject,{
+				timeout:3000
+			});
+			
+			var s = JSON.parse(res.getBody());
+			console.log(s);
+			response.say(s);
+		}catch(e){
+			console.log(e.message);
+			response.say('Sorry, Some error occured ');
+		}
+      	return true;
 	}
     	
 
